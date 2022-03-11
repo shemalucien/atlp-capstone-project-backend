8f077fd86b377c5407a3cea39fdcdf8c21ba80b4
shemalucien <shemalucien5@gmail.com>
2022-03-11 10:52:17 +0200

    heroku deploy

--- src/database/index.js
+++ src/database/index.js
@@ -1,21 +1,45 @@
-import mongoose from 'mongoose';
-import "dotenv/config";
+// import mongoose from 'mongoose';
+// import "dotenv/config";
 
-const enviroment = process.env.NODE_ENV;
-// database url according to environment 
+// const enviroment = process.env.NODE_ENV;
+
+
 // const dev_db_url = process.env.DEVELOPMENT_DB;
 // const prod_db_url = process.env.PRODUCTION_DB;
 // const test_db_url = process.env.TEST_DB;
 
 // const connectionUrl = (enviroment == 'dev') ? dev_db_url : (enviroment == 'prod') ? prod_db_url : test_db_url;
 
-mongoose.connect("mongodb+srv://shemalucien:Shema123@articles.2pt7v.mongodb.net/PortfolioDb", {
+// mongoose.connect( dev_db_url, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
+//     .then(() => {
+//         console.log("App connected to Mongodb successfully")
+//     })
+//     .catch((e) => {
+//         console.log("Mongodb connection error " + e.message);
+//     })
+
+import mongoose from 'mongoose';
+import "dotenv/config";
+const enviroment = process.env.NODE_ENV;
+// database url according to environment 
+const dev_db_url = process.env.DEVELOPMENT_DB;
+const prod_db_url = process.env.PRODUCTION_DB;
+const test_db_url = process.env.TEST_DB;
+
+const connectionUrl = (enviroment=='dev') ? dev_db_url : (enviroment == 'prod') ? prod_db_url : test_db_url;
+console.log("env: "+enviroment+"  ---- url: "+connectionUrl)
+
+
+mongoose.connect(connectionUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-    .then(() => {
-        console.log("App connected to Mongodb successfully")
-    })
-    .catch((e) => {
-        console.log("Mongodb connection error " + e.message);
-    })
+.then(() => {
+    console.log("App connected to Mongodb successfully")
+})
+.catch((e) => {
+    console.log("Mongodb connection error "+e.message);
+})
