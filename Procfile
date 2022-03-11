a3875a6ff980ab104cf18ba3544028748b4f8e56
shemalucien <shemalucien5@gmail.com>
2022-03-11 14:46:47 +0200

    added srcipts

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: node src/index.js
+web: babel-node src/index.js
 
--- src/database/index.js
+++ src/database/index.js
@@ -1,26 +1,3 @@
-// import mongoose from 'mongoose';
-// import "dotenv/config";
-
-// const enviroment = process.env.NODE_ENV;
-
-
-// const dev_db_url = process.env.DEVELOPMENT_DB;
-// const prod_db_url = process.env.PRODUCTION_DB;
-// const test_db_url = process.env.TEST_DB;
-
-// const connectionUrl = (enviroment == 'dev') ? dev_db_url : (enviroment == 'prod') ? prod_db_url : test_db_url;
-
-// mongoose.connect( dev_db_url, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// })
-//     .then(() => {
-//         console.log("App connected to Mongodb successfully")
-//     })
-//     .catch((e) => {
-//         console.log("Mongodb connection error " + e.message);
-//     })
-
 import mongoose from 'mongoose';
 import "dotenv/config";
 const enviroment = process.env.NODE_ENV;
