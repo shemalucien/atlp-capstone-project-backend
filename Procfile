cdb076079920f7bd92836a37f8b2273d04648245
shemalucien <shemalucien5@gmail.com>
2022-03-11 08:23:39 +0200

    initial commit

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: babel-node src/index.js
-
--- src/routes/subscriber.route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { subscribe, unsubscribe, getAllSubscribers } from '../controllers/subscribers.controller';
-import { checkAdminAuth } from '../middleware/check-users';
-const router = express.Router();
-router.post('/subscribe', subscribe);
-router.delete('/unsubscribe', unsubscribe);
-router.get('/',checkAdminAuth,getAllSubscribers);
-export default router;
