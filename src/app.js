c088d4ec19e94ac4dcfff998559ae2ea6e40ca89
shemalucien <shemalucien5@gmail.com>
2022-03-14 16:54:36 +0200

    (chore-api-documentations): added documentations of blogs,queries and users

--- src/app.js
+++ src/app.js
@@ -1,20 +1,25 @@
 import "./database";
 import express from 'express';
-
+//swagger-ui
+import cors from "cors";
+import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
+import swaggerDoc from "./db.json";
 import queryRoutes from './routes/query.route';
 import authRoutes from './routes/auth.route';
 import blogRoutes from './routes/blog.route';
 import commentRoutes from './routes/comment.route';
 import subscribeRoutes from './routes/subscriber.route';
 
+
 const server = express();
+
+
+
 // default route
 server.get('/', (req, res) => {
-    res.status(200).json({ success: true, message: "You successfully landed on My Portfolio API" })
+	res.status(200).json({ success: true, message: "You successfully landed on My Portfolio API" })
 });
-
-
-
 server.use(express.json());
 
 server.use('/api/v1/queries', queryRoutes);
@@ -23,4 +28,21 @@ server.use('/api/v1/blogs', blogRoutes);
 server.use('/api/v1/comments', commentRoutes);
 server.use('/api/v1/subscribers', subscribeRoutes);
 
+
+
 export default server;
+
+server.use(cors());
+server.use(morgan("dev"));
+server.use("/api/v1/", authRoutes);
+server.use(
+	"/api-docs",
+	swaggerUi.serve,
+	swaggerUi.setup(swaggerDoc, { explorer: true })
+);
+
+server.use("*", (req, res, next) => {
+	res.status(404).json({
+		error: "NOT FOUND",
+	});
+});
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 
 import app from './app';
 import 'dotenv/config';
-const port = process.env.port;
+const port=process.env.port;
+
 
 app.listen(port, () => { console.log("Server listening on port " + port) });
