f44baa995bf58d0419fc05635e6095e2eaf1511f
shemalucien <shemalucien5@gmail.com>
2022-03-17 13:59:52 +0200

    users profile

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 import app from './app';
 import 'dotenv/config';
-const port=process.env.port;
+const PORT = process.env.PORT || 5000;
 
 
-app.listen(port, () => { console.log("Server listening on port " + port) });
+app.listen(PORT, () => { console.log("Server listening on port " + PORT) });
