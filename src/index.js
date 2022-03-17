f099b21e7664c61a2804d7d2184dbc0a2747eeab
shemalucien <shemalucien5@gmail.com>
2022-03-17 09:57:16 +0200

    fixing port2

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import app from './app.js';
 import 'dotenv/config';
 const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => { console.log("Server listening on port " + port) });
+app.listen(PORT, () => { console.log("Server listening on port " + PORT) });
