8d2d83acfe4b8a52b5ac9df1120e80dc8c72eddc
shemalucien <shemalucien5@gmail.com>
2022-03-21 12:45:01 +0200

    fixed queries1

--- src/tests/e2e/queries.test.js
+++ src/tests/e2e/queries.test.js
@@ -28,67 +28,140 @@ describe('POST API /api/v1/queries', () => {
                 return done();
             })
     });
-    it('it should get all the queries and return 200', (done) => {
+
+})
+describe('POST API /api/v1/auth/login', () => {
+    before(() => {
+        mongoose.connection.dropCollection('login');
+    })
+    const user = {
+        email: "shemalucien5@gmail.com",
+        password: "123456"
+    }
+    const query = {
+        "name": "shemalucien",
+        "email": "shemalucien5@gmail.com",
+        "message": "message is sent"
+    }
+    let token = "";
+    it('it should successfully login and return 200', (done) => {
         chai.request(app)
-            .get('/api/v1/queries')
+            .post('/api/v1/auth/login')
+            .set("Authorization", `Bearer ${token}`)
+            .send(user)
             .end((err, res) => {
                 if (err) return done(err)
+                token = res.body.token;
                 expect(res.status).to.be.equal(200);
-                expect(res.body).to.be.a('object');
+                expect(res.body).to.have.property("token");
                 return done();
             })
-    });
-
-
-
-    let queryId;
-    it('it should get the query using its id and return 200', (done) => {
-        chai.request(app)
-            .post('/api/v1/queries')
-            .send(query)
-            .end((err, res) => {
-                if (err) return done(err)
-                queryId = res.body.data._id;
-                chai.request(app).get('/api/v1/queries/' + queryId)
-                    .end((err, res) => {
-                        if (err) return done(err);
-                        expect(res.status).to.be.equal(200)
-                        return done();
-                    })
+        describe('POST API /api/v1/queries', () => {
+            before(() => {
+                mongoose.connection.dropCollection('queries');
             })
-    })
-    it('it should delete the query using its id and return 200', (done) => {
-        chai.request(app)
-            .post('/api/v1/queries')
-            .send(query)
-            .end((err, res) => {
-                if (err) return done(err)
-                queryId = res.body.data._id;
+            const query = {
+                "name": "shemalucien",
+                "email": "shemalucien5@gmail.com",
+                "message": "message is sent"
+            }
+
+            it('it should get all the queries and return 200', (done) => {
                 chai.request(app)
-                    .delete('/api/v1/queries/' + queryId)
+                    .get('/api/v1/queries')
+                    .set("Authorization", `Bearer ${token}`)
                     .end((err, res) => {
-                        if (err) return done(err);
-                        expect(res.status).to.be.equal(200)
-                        return done();
-                    })
-            })
-    })
-    it('it should update the query using its id and return 200', (done) => {
-        chai.request(app)
-            .post('/api/v1/queries')
-            .send(query)
-            .end((err, res) => {
-                if (err) return done(err)
-                queryId = res.body.data._id;
-                chai.request(app)
-                    .put('/api/v1/queries/' + queryId)
-                    .end((err, res) => {
-                        if (err) return done(err);
-                        expect(res.status).to.be.equal(200)
+                        if (err) return done(err)
+                        expect(res.status).to.be.equal(200);
                         expect(res.body).to.be.a('object');
                         return done();
                     })
+            });
+        });
+        describe('POST API /api/v1/queries', () => {
+            before(() => {
+                mongoose.connection.dropCollection('queries');
             })
+            const query = {
+                "name": "shemalucien",
+                "email": "shemalucien5@gmail.com",
+                "message": "message is sent"
+            }
+            let queryId;
+            it('it should get the query using its id and return 200', (done) => {
+                chai.request(app)
+                    .post('/api/v1/queries')
+                    .set("Authorization", `Bearer ${token}`)
+                    .send(query)
+                    .end((err, res) => {
+                        if (err) return done(err)
+                        queryId = res.body.data._id;
+                        chai.request(app).get('/api/v1/queries/' + queryId)
+                            .end((err, res) => {
+                                if (err) return done(err);
+                                expect(res.status).to.be.equal(200)
+                                return done();
+                            })
+                    })
+            });
+        });
+        describe('POST API /api/v1/queries', () => {
+            before(() => {
+                mongoose.connection.dropCollection('queries');
+            })
+            const query = {
+                "name": "shemalucien",
+                "email": "shemalucien5@gmail.com",
+                "message": "message is sent"
+            }
+            let queryId;
+            it('it should delete the query using its id and return 200', (done) => {
+                chai.request(app)
+                    .post('/api/v1/queries')
+                    .set("Authorization", `Bearer ${token}`)
+                    .send(query)
+                    .end((err, res) => {
+                        if (err) return done(err)
+                        queryId = res.body.data._id;
+                        chai.request(app)
+                            .delete('/api/v1/queries/' + queryId)
+                            .end((err, res) => {
+                                if (err) return done(err);
+                                expect(res.status).to.be.equal(200)
+                                return done();
+                            })
+                    })
+            });
+        });
+        describe('POST API /api/v1/queries', () => {
+            before(() => {
+                mongoose.connection.dropCollection('queries');
+            })
+            const query = {
+                "name": "shemalucien",
+                "email": "shemalucien5@gmail.com",
+                "message": "message is sent"
+            }
+            let queryId;
+            it('it should update the query using its id and return 200', (done) => {
+                chai.request(app)
+                    .post('/api/v1/queries')
+                    .send(query)
+                    .end((err, res) => {
+                        if (err) return done(err)
+                        queryId = res.body.data._id;
+                        chai.request(app)
+                            .put('/api/v1/queries/' + queryId)
+                            .set("Authorization", `Bearer ${token}`)
+                            .end((err, res) => {
+                                if (err) return done(err);
+                                expect(res.status).to.be.equal(200)
+                                expect(res.body).to.be.a('object');
+                                return done();
+                            })
+                    })
+            });
+        });
+
     })
 })
-
